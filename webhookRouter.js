74fc24bf7d973556aeb89f6396a008f6c6d394c4
Nicholas Marino <nsmarino0@gmail.com>
2020-08-23 09:44:54 -0400

    deploy for heroku server

--- webhookRouter.js
+++ webhookRouter.js
@@ -1,14 +1,12 @@
 const webhookRouter = require('express').Router()
 const fetch = require('node-fetch');
 
-
-// i think this works but im unsure how to fully test it without deploying
 webhookRouter.post('/', async (request, response) => {
   try {
     const {
       data: { object: paymentIntent },
     } = request.body;
-
+    console.log(request.body)
     const printfulOrderId = paymentIntent.description
     console.log(printfulOrderId)
     await fetch(`https://api.printful.com/orders/${printfulOrderId}/confirm`, {
